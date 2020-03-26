11229856b7deb1730098266c1cd3fb618987842f
James Bradley <jhbradley@mitre.org>
2020-03-26 12:07:19 -0400

    FPG make call with ci-tool param

--- travis/test_measures.sh
+++ travis/test_measures.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd fhir-patient-generator
+git checkout ci-tool-makefile-param
+make CI_TOOL=TRUE
 
 # Use make commands to test measures and/or
 # compare calculation results against the
