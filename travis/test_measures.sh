d7c59f9ca9bcf2784060e3ebee04bcd15efc841e
jbradl11 <jhbradley@mitre.org>
2020-03-27 10:21:19 -0400

    Update test_measures.sh

--- travis/test_measures.sh
+++ travis/test_measures.sh
@@ -2,7 +2,7 @@
 
 cd fhir-patient-generator
 git checkout ci-tool-makefile-param
-make #CI_TOOL=TRUE
+make CI_TOOL=TRUE
 
 # Use make commands to test measures and/or
 # compare calculation results against the
