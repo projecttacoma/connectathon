9ad414e7459461729a48fc131f292ac4fc9c8fe7
Chris Hossenlopp <hossenlopp@mitre.org>
2020-04-21 23:30:53 -0400

    First pass at measure calculation and report compare. Only runs EXM_124. This should fail due to connectathon patients that polluted the f-p-g measure report when it was created.

--- travis/calculateMeasures.js
+++ travis/calculateMeasures.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
 const http = require('http');
+const process = require('process')
+const measureReportCompare = require('./measureReportCompare');
+
+const PERIOD_START = '2019-01-01';
+const PERIOD_END = '2019-12-31';
 
 async function getCQFMeasureList() {
   return new Promise((resolve, reject) => {
@@ -44,10 +49,15 @@ async function getTestMeasureList() {
   let fpgDir = fs.readdirSync('./fhir-patient-generator/')
   let applicableMeasuresDirs = fpgDir.filter((dir) => { return dir.startsWith("EXM_") });
   let measureDirInfo = applicableMeasuresDirs.map((measureDir) => {
-    return {
+    let testDirInfo = {
       exmId: measureDir,
       path: `./fhir-patient-generator/${measureDir}/patients-r4`
+    };
+    let measureReportFile = fs.readdirSync(testDirInfo.path).find((filename) => { return filename.includes('measure-report')});
+    if (measureReportFile) {
+      testDirInfo.measureReportPath = `${testDirInfo.path}/${measureReportFile}`;
     }
+    return testDirInfo;
   });
 
   return measureDirInfo;
@@ -59,16 +69,19 @@ async function loadTestDataFolder(testDataFolder) {
     .filter((dir) => { return dir.isDirectory() })
     .map((dir) => { return dir.name })
 
+  let bundleResourceInfos = [];
   for (let subfolder of subfolders) {
     let subfolderPath = testDataFolder + '/' + subfolder;
-    console.log(subfolderPath);
     let patientBundles = fs.readdirSync(subfolderPath)
     for (let patientBundleName of patientBundles) {
       if (!patientBundleName.endsWith(".json")) continue;
-      console.log(`${subfolderPath}/${patientBundleName}`)
-      await loadPatientBundle(`${subfolderPath}/${patientBundleName}`)
+      console.log(`Loading bundle ${subfolderPath}/${patientBundleName}`)
+      let newResourceInfo = await loadPatientBundle(`${subfolderPath}/${patientBundleName}`)
+      bundleResourceInfos.push(newResourceInfo);
     }
   }
+
+  return bundleResourceInfos;
 }
 
 async function loadPatientBundle(patientBundlePath) {
@@ -83,9 +96,21 @@ async function loadPatientBundle(patientBundlePath) {
         if (res.statusCode != 200) {
           reject(`Status code ${res.statusCode} was unexpected when posting bundle.`);
           return;
-        } else {
-          resolve();
         }
+
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+          let transactionResult = JSON.parse(rawData);
+          let resourcesCreated = transactionResult.entry.map((entry) => {
+            return entry.response.location.replace("/_history/1", '');
+          })
+          resolve({
+            originalBundle: patientBundlePath,
+            resources: resourcesCreated
+          });
+        });
       });
 
     let bundleStream = fs.createReadStream(patientBundlePath);
@@ -93,13 +118,158 @@ async function loadPatientBundle(patientBundlePath) {
   });
 }
 
-async function calculateMeasures() {
+async function deleteBundleResources(bundleResourceInfos) {
+  for (let bundleResourceInfo of bundleResourceInfos) {
+    console.log("Deleting resources from " + bundleResourceInfo.originalBundle)
+    await deleteResources(bundleResourceInfo.resources);
+  }
+}
+
+async function deleteResources(resourcesToDelete) {
+  let deleteTransaction = {
+    resourceType: 'Bundle',
+    id: 'big-delete',
+    type: 'transaction',
+    entry: resourcesToDelete.map((resource) => {
+      return {
+        request: {
+          method: 'DELETE',
+          url: resource
+        }
+      };
+    })
+  };
+
+  return new Promise((resolve, reject) => {
+    let req = http.request("http://localhost:8080/cqf-ruler-r4/fhir",
+      {
+        method: 'POST',
+        headers: {
+          "Content-Type": 'application/json'
+        }
+      }, (res) => {
+        if (res.statusCode != 200) {
+          reject(`Status code ${res.statusCode} was unexpected when deleting data.`);
+          return;
+        } else {
+          resolve();
+        }
+      });
+    req.write(JSON.stringify(deleteTransaction));
+    req.end();
+  });
+}
+
+async function getFakeMeasureReport(measureId) {
+  return loadReferenceMeasureReport('./travis/exm124-mr.json');
+}
+
+async function loadReferenceMeasureReport(measureReportPath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(measureReportPath, (err, data) => {
+      if (err) reject(err);
+      resolve(JSON.parse(data));
+    });
+  });
+}
+
+async function getMeasureReport(measureId) {
+  return new Promise((resolve, reject) => {
+    console.log(`Executing measure ${measureId}`);
+    let req = http.request(`http://localhost:8080/cqf-ruler-r4/fhir/Measure/${measureId}/$evaluate-measure?reportType=patient-list&periodStart=${PERIOD_START}&periodEnd=${PERIOD_END}`,
+      {
+        method: 'GET',
+        timeout: 480 //8 minute timeout because this is slooooow
+      }, (res) => {
+        if (res.statusCode != 200) {
+          reject(`Status code ${res.statusCode} was unexpected when posting bundle.`);
+          return;
+        } else {
+          res.setEncoding('utf8');
+          let rawData = '';
+          res.on('data', (chunk) => { rawData += chunk; });
+          res.on('end', () => {
+            resolve(JSON.parse(rawData));
+          });
+        }
+      }
+    );
+
+    req.on('error', (e) => {
+      console.error(e);
+      reject(e);
+    });
+    req.end();
+  });
+}
+
+
+
+async function calculateMeasuresAndCompare() {
+  let measureDiffInfo = [];
   let cqfMeasures = await getCQFMeasureList();
   console.log(cqfMeasures);
   let testPatientMeasures = await getTestMeasureList();
   console.log(testPatientMeasures);
-  loadTestDataFolder('./fhir-patient-generator/EXM_124/patients-r4');
+
+  for (let testPatientMeasure of testPatientMeasures) {
+    if (testPatientMeasure.exmId != 'EXM_124') continue; // TODO remove this when ready to try with all measures
+
+    let cqfMeasure = cqfMeasures.find((measure) => measure.exmId == testPatientMeasure.exmId)
+    if (!cqfMeasure) {
+      console.log(`Measure ${testPatientMeasure.exmId} not found in cqf-ruler.`)
+    }
+
+    console.log(`Loading test data for ${testPatientMeasure.exmId}`);
+    let bundleResourceInfos = await loadTestDataFolder(testPatientMeasure.path);
+
+    let report = await getMeasureReport(cqfMeasure.id)
+    let referenceReport = await loadReferenceMeasureReport(testPatientMeasure.measureReportPath);
+
+    let badPatients = measureReportCompare.compareMeasureReports(referenceReport, report)
+
+    measureDiffInfo.push({
+      exmId: testPatientMeasure.exmId,
+      badPatients: badPatients
+    })
+
+    console.log(`Removing test data for ${testPatientMeasure.exmId}`);
+    await deleteBundleResources(bundleResourceInfos);
+  }
+
+  return measureDiffInfo;
 }
 
 
-calculateMeasures();
+calculateMeasuresAndCompare()
+  .then((measureDiffInfo) => {
+    console.log();
+    console.log("--- RESULTS ---");
+    console.log();
+    let hasDifferences = false;
+
+    measureDiffInfo.forEach((measureDiff) => {
+      console.log(`MEASURE ${measureDiff.exmId}`);
+      if (measureDiff.badPatients.length > 0) {
+        hasDifferences = true;
+        measureDiff.badPatients.forEach((patient) => {
+          console.log(`|- ${patient.patientName}`);
+          patient.issues.forEach((issue) => {
+            console.log(`|   ${issue}`)
+          });
+        });
+      } else {
+        console.log("  No Issues!");
+      }
+      console.log();
+    });
+
+
+    if (hasDifferences) {
+      process.exit(1);
+    }
+  })
+  .catch((reason) => {
+    console.error(reason);
+    process.exit(2);
+  });
---
+++ travis/measureReportCompare.js
@@ -0,0 +1,76 @@
+
+function findCorrespondingGroup(referenceGroup, report) {
+  return report.group.find((group) => {
+    return referenceGroup.id == group.id
+  });
+}
+
+function findCorrespondingPopulation(referencePopulation, group) {
+  return group.population.find((population) => {
+    return referencePopulation.code.coding[0].code == population.code.coding[0].code
+  });
+}
+
+function grabReferencedResource(reference, report) {
+  let id = reference.replace('#', '');
+  return report.contained.find((resource) => { return resource.id = id });
+}
+
+function addBadPatientEntry(badPatientsList, patientName, issueMessage) {
+  let badPatient = badPatientsList.find((badPatient) => badPatient.patientName == patientName );
+  if (!badPatient) {
+    badPatient = { patientName: patientName, issues: [] };
+    badPatientsList.push(badPatient);
+  }
+
+  badPatient.issues.push(issueMessage);
+}
+
+
+function compareMeasureReports(referenceReport, report) {
+  let badPatientsList = []
+
+  console.log(`Comparing reports for ${referenceReport.measure}`);
+
+  // iterate groups in referenceReport
+  referenceReport.group.forEach(referenceGroup => {
+
+    console.log(`  Comparing group: ${referenceGroup.id}`)
+    // find corresponding group in report
+    let group = findCorrespondingGroup(referenceGroup, report);
+
+    // iterate populations
+    referenceGroup.population.forEach(referencePopulation => {
+
+      console.log(`    Comparing population: ${referencePopulation.code.coding[0].display}`)
+      // find corresponding population
+      let population = findCorrespondingPopulation(referencePopulation, group);
+
+      // grab lists of patients
+      let referenceList = grabReferencedResource(referencePopulation.subjectResults.reference, referenceReport);
+      let list = grabReferencedResource(population.subjectResults.reference, report);
+      let referencePatientNames = referenceList.entry.map((entry) => { return entry.item.display; });
+      let patientNames = list.entry.map((entry) => { return entry.item.display; });
+
+      // compare lists
+      let missingPatients = referencePatientNames.filter((patientName) => { return !patientNames.includes(patientName) });
+      let unexpectedPatients = patientNames.filter((patientName) => { return !referencePatientNames.includes(patientName) });
+
+      console.log(`      Expected ${referencePatientNames.length} - Actual ${patientNames.length}`);
+      missingPatients.forEach(patientName => {
+        console.log(`        MISSING    ${patientName}`);
+        addBadPatientEntry(badPatientsList, patientName, `Missing from ${referenceGroup.id} - ${referencePopulation.code.coding[0].display}`)
+      });
+      unexpectedPatients.forEach(patientName => {
+        console.log(`        UNEXPECTED ${patientName}`);
+        addBadPatientEntry(badPatientsList, patientName, `Unexpected in ${referenceGroup.id} - ${referencePopulation.code.coding[0].display}`)
+      });
+      // build list of patients and their membership as we do this
+    });
+  });
+
+  // return list of patients
+  return badPatientsList;
+}
+
+module.exports.compareMeasureReports = compareMeasureReports;
