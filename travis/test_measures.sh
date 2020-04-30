02296f64e05e4cb099219e8d44d20ec78a56a885
Chris Hossenlopp <hossenlopp@mitre.org>
2020-04-30 13:25:40 -0400

    Post rebase cleanup. This should pass in travis.

--- travis/test_measures.sh
+++ travis/test_measures.sh
@@ -5,8 +5,6 @@ if ! ./travis/rebuild_bundles.sh; then
   exit 1
 fi
 
-#cd fhir-patient-generator
-
 # Use make commands to test measures and/or
 # compare calculation results against the
 # existing patient sets.
