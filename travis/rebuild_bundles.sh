47afac6e467c5807340f7a0471c9bbf10212d274
Chris Hossenlopp <hossenlopp@mitre.org>
2020-09-09 18:45:03 -0400

    removed rebuild option that included patients in bundles

--- travis/rebuild_bundles.sh
+++ travis/rebuild_bundles.sh
@@ -8,7 +8,7 @@ ig_resource_path=./fhir401/input/connectathon.xml
 #rm ./fhir401/input/resources/measure/measure-EXM165_FHIR4-8.5.000.json
 rm ./fhir401/input/pagecontent/cql/EXM165-9.0.000.cql
 
-java -jar $tooling_jar -RefreshIG -ip="./fhir401/" -ini="./fhir401/ig.ini" -t -d -p -v 2>&1 -fs http://localhost:8080/cqf-ruler-r4/fhir | tee $rebuild_log
+java -jar $tooling_jar -RefreshIG -ip="./fhir401/" -ini="./fhir401/ig.ini" -t -d -v 2>&1 -fs http://localhost:8080/cqf-ruler-r4/fhir | tee $rebuild_log
 
 if grep -q '^0 Measures failed refresh' $rebuild_log && grep -q '^0 Measures refreshed, but not bundled' $rebuild_log; then
   echo "Bundle rebuild appears to be successful"
