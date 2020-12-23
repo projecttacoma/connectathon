a8ed0508b83cbcce958e8f6fbca02017e98deace
Chris Hossenlopp <hossenlopp@mitre.org>
2020-12-23 12:14:15 -0500

    updated CI tool to use tooling 1.3.1

--- travis/rebuild_bundles.sh
+++ travis/rebuild_bundles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tooling_jar=./travis/tooling/tooling-1.3.0-SNAPSHOT-jar-with-dependencies.jar
+tooling_jar=./travis/tooling/tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
 rebuild_log=./travis/tooling/rebuildRun.log
 ig_resource_path=./fhir401/input/connectathon.xml
 
@@ -8,7 +8,7 @@ ig_resource_path=./fhir401/input/connectathon.xml
 #rm ./fhir401/input/resources/measure/measure-EXM165_FHIR4-8.5.000.json
 rm ./fhir401/input/pagecontent/cql/EXM165-9.0.000.cql
 
-java -jar $tooling_jar -RefreshIG -ip="./fhir401/" -ini="./fhir401/ig.ini" -t -d -v 2>&1 -fs http://localhost:8080/cqf-ruler-r4/fhir | tee $rebuild_log
+java -jar $tooling_jar -RefreshIG -ini="./fhir401/ig.ini" -t -d -v 2>&1 -fs http://localhost:8080/cqf-ruler-r4/fhir | tee $rebuild_log
 
 if grep -q '^0 Measures failed refresh' $rebuild_log && grep -q '^0 Measures refreshed, but not bundled' $rebuild_log; then
   echo "Bundle rebuild appears to be successful"
